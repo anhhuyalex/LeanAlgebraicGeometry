d5fbf2a84a6e92d5b3d79fd9a99dee3ae010eb11
anhhuyalex <alexn@minerva.kgi.edu>
2026-07-08 19:28:12 -0400

    chore: cleanup

--- MainProjects/Algebraic-Jacobian-Challenge/fix_lines.sh
+++
@@ -1,6 +0,0 @@
-sed -i -e '367,367c\
-  have h2 : (Pic0Scheme C).left.residueField ((identitySection C).base default) ⟶ CommRingCat.of k := sorry\
-  have h3 : CommRingCat.of k ⟶ (Pic0Scheme C).left.residueField ((identitySection C).base default) := sorry\
-  have h4 : h2 ≫ h3 = 𝟙 _ := sorry\
-  have h5 : h3 ≫ h2 = 𝟙 _ := sorry\
-  exact ⟨Iso.mk h2 h3 h4 h5⟩' AlgebraicJacobian/Picard/Pic0AbelianVariety.lean
